b1cddd711faef769486a04e7764fc6fcbe6a179b
Muhammad Riziq Zhapran <134038828+nicwaproject@users.noreply.github.com>
2024-10-30 16:52:59 +0800

    Update script.js

--- script.js
+++ script.js
@@ -92,7 +92,10 @@ window.addEventListener('resize', function() {
   function updateCountdown() {
       const now = new Date();
       const timeDiff = targetDate - now;
-
+    if (timeDiff <= 0) {
+        clearInterval(intervalId);
+        timeDiff = 0;
+    }
       const days = Math.floor(timeDiff / (1000 * 60 * 60 * 24));
       const hours = Math.floor((timeDiff % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
       const minutes = Math.floor((timeDiff % (1000 * 60 * 60)) / (1000 * 60));
